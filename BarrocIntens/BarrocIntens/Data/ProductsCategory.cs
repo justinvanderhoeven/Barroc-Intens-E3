8bd68b8e8d52abf3945d842ebdfc78965311f363
Remonit <scribb22m@gmail.com>
2023-11-06 22:41:47 +0100

    focking spelfouten
    godverdomme dani wat kan jij selcht typen ProductCatergory

--- BarrocIntens/BarrocIntens/Data/ProductsCategory.cs
+++ BarrocIntens/BarrocIntens/Data/ProductsCategory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BarrocIntens.Data
 {
-    internal class ProductsCatergory
+    internal class ProductsCategory
     {
         public int Id { get; set; }
         public string Name { get; set; }
