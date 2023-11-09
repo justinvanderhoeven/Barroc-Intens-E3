3f8999b1e2a9cd9ac39a74a357e547d64133603a
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2023-11-09 14:07:41 +0100

    !Half Coded!
    Missing code for #30. Ticket not done

--- BarrocIntens/BarrocIntens/LoginPage.xaml.cs
+++ BarrocIntens/BarrocIntens/LoginPage.xaml.cs
@@ -2,16 +2,22 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
+using BarrocIntens.Data;
+using System.Text;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using static System.Net.Mime.MediaTypeNames;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,11 +32,37 @@ namespace BarrocIntens
         public LoginPage()
         {
             this.InitializeComponent();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(FinancePage));
+            // ALS LOGIN SUCCESVOL 
+            // Frame.Navigate(typeof(FinancePage));
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Password;
+
+            using (var db = new AppDbContext())
+            {
+                var user = db.Users.FirstOrDefault(u => u.Username == username);
+
+                if (user != null && VerifyPassword(password, user.Password))
+                {
+                    MessageBox.Show("Login successful!");
+                    // You can navigate to another page or perform other actions upon successful login.
+                    Frame.Navigate(typeof(FinancePage));
+                }
+                else
+                {
+                    MessageBox.Show("Login failed. Please check your credentials.");
+                }
+            }
+
         }
     }
 }
