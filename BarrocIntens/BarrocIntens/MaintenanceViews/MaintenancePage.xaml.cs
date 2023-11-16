f5c47a85c06ac471a78b7a6e08e4b4f574a8303e
Daniel-vdL <alexanderdanailin@gmail.com>
2023-11-16 11:59:21 +0100

    MaintenancePage Update

--- BarrocIntens/BarrocIntens/MaintenanceViews/MaintenancePage.xaml.cs
+++ BarrocIntens/BarrocIntens/MaintenanceViews/MaintenancePage.xaml.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -16,7 +19,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace BarrocIntens
+namespace BarrocIntens.MaintenanceViews
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
