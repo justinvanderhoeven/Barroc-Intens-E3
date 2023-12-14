e6d764f19ec515cdbb910f75ab136f291e3690d9
Remonit <scribb22m@gmail.com>
2023-12-14 15:21:53 +0100

    create customer form

--- BarrocIntens/BarrocIntens/SalesViews/AddCustomerView.xaml.cs
+++ BarrocIntens/BarrocIntens/SalesViews/AddCustomerView.xaml.cs
@@ -1,3 +1,4 @@
+using BarrocIntens.Data;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -23,9 +24,81 @@ namespace BarrocIntens.SalesViews
     /// </summary>
     public sealed partial class AddCustomerView : Page
     {
+        public Company CurrentCompany { get; set; }
         public AddCustomerView()
         {
             this.InitializeComponent();
+            using var db = new AppDbContext();
+            CompanySuggestBox.ItemsSource = db.Companies.Where(c => c.ContactId == null).ToList();
+        }
+
+        private void CompanySuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            if (args.SelectedItem is Company selectedCompany)
+            {
+                CompanySuggestBox.Text = selectedCompany.Name;
+                CurrentCompany = selectedCompany;
+                checkForInput();
+            }
+            else
+            {
+                CompanySuggestBox.Text = "No selection";
+            }
+        }
+
+        private void AddCustomerButton_Click(object sender, RoutedEventArgs e)
+        {
+            User newCustomer = new User
+            {
+                Name = nameInput.Text,
+                Email = emailInput.Text,
+                Password = "wachtwoord",
+                DepartmentId = 1
+            };
+            using (var db = new AppDbContext())
+            {
+                db.Users.Add(newCustomer);
+                db.SaveChanges();
+                nameInput.Text = string.Empty;
+                emailInput.Text = string.Empty;
+                CompanySuggestBox.Text = string.Empty;
+            }
+            using (var db = new AppDbContext())
+            {
+                db.Attach(CurrentCompany);
+                User addedUser = db.Users.FirstOrDefault(u => u.Name == newCustomer.Name && u.Email == newCustomer.Email && u.DepartmentId == newCustomer.DepartmentId);
+                CurrentCompany.ContactId = addedUser.Id;
+                db.SaveChanges();
+            }
+        }
+
+        public void checkForInput()
+        {
+            if (nameInput.Text == "")
+            {
+                AddCustomerButton.IsEnabled = false;
+                return;
+            }
+            if(emailInput.Text == "")
+            {
+                AddCustomerButton.IsEnabled = false;
+                return;
+            }
+            if(CurrentCompany == null)
+            {
+                return;
+            }
+            AddCustomerButton.IsEnabled = true;
+        }
+
+        private void nameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            checkForInput();
+        }
+
+        private void emailInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            checkForInput();
         }
     }
 }
