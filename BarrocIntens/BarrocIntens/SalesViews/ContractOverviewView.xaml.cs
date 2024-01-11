6a73462adadc710a2a0f293447e3a4f58d0d3849
Remonit <scribb22m@gmail.com>
2024-01-11 16:11:38 +0100

    overview

--- BarrocIntens/BarrocIntens/SalesViews/ContractOverviewView.xaml.cs
+++ BarrocIntens/BarrocIntens/SalesViews/ContractOverviewView.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using BarrocIntens.Data;
+using Microsoft.EntityFrameworkCore;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +25,39 @@ namespace BarrocIntens.SalesViews
     /// </summary>
     public sealed partial class ContractOverviewView : Page
     {
+
         public ContractOverviewView()
         {
             this.InitializeComponent();
+            using var db = new AppDbContext();
+            var contracts = db.Contracts.Include(c => c.ContractProducts).Include(c => c.Company)
+               .ToList();
+            contractListView.ItemsSource = contracts;
+        }
+
+        private void deleteCompany_Click(object sender, RoutedEventArgs e)
+        {
+            if (contractListView.SelectedItem is Contract selectedContract)
+            {
+                using var db = new AppDbContext();
+                db.Contracts.Remove(selectedContract);
+                db.SaveChanges();
+
+                contractListView.ItemsSource = db.Contracts.Include(c => c.ContractProducts).Include(c => c.Company)
+               .ToList();
+            }
+        }
+
+        private void addCompany_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(AddContractView));
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchInput = searchTextbox.Text;
+
+            using var db = new AppDbContext();
+            contractListView.ItemsSource = db.Companies.Where(m => m.Name.Contains(searchInput));
         }
     }
 }
