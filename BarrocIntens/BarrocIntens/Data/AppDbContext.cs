5889e91f7ce2ea81ea8a603959002e9e4426e3db
Remonit <scribb22m@gmail.com>
2024-01-11 15:46:19 +0100

    add company

--- BarrocIntens/BarrocIntens/Data/AppDbContext.cs
+++ BarrocIntens/BarrocIntens/Data/AppDbContext.cs
@@ -283,7 +283,8 @@ namespace BarrocIntens.Data
                 {
                     Id = 1,
                     CompanyId = 1,
-                    ActiveUntil = DateOnly.FromDateTime( DateTime.UtcNow ),
+                    StartDate = DateOnly.FromDateTime( DateTime.UtcNow ),
+                    EndDate = DateOnly.FromDateTime(DateTime.UtcNow),
                 }
             );
 
@@ -294,7 +295,6 @@ namespace BarrocIntens.Data
                     ContractId = 1,
                     ProductId = 1,
                     Amount = 5,
-                    PricePerProduct = 5,
                 }
             );
 
