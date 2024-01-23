9ac00f9808f1be08cfa1e6a7e81791f62855b10e
Natebuilder <d272226@edu.curio.nl>
2024-01-23 15:03:01 +0100

    maintenance kan product gebruiken

---
+++ BarrocIntens/BarrocIntens/MaintenanceViews/ProductStockView.xaml.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using BarrocIntens.Data;
+using BarrocIntens.PurchaseViews;
+using Microsoft.EntityFrameworkCore;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace BarrocIntens.MaintenanceViews
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ProductStockView : Page
+    {
+        public ProductStockView()
+        {
+            this.InitializeComponent();
+
+            using (var db = new AppDbContext())
+            {
+                ProductSearchingView.ItemsSource = db.Products.ToList();
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchInput = searchTextBox.Text;
+
+            using var db = new AppDbContext();
+            ProductSearchingView.ItemsSource = db.Products.Where(m => m.Name.Contains(searchInput));
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            using (var db = new AppDbContext())
+            {
+                ProductSearchingView.ItemsSource = db.Products.ToList();
+            }
+        }
+
+        private void ProductSearchView_ItemClick(object sender, ItemClickEventArgs p)
+        {
+            var SelectedItem = (Product)p.ClickedItem;
+            var window = new ProductUsingView(SelectedItem.Id);
+            window.Activate();
+        }
+
+        private void Ov_Checked(object sender, RoutedEventArgs e)
+        {
+            if (StockCheckBox.IsChecked == true)
+            {
+                using var db = new AppDbContext();
+
+                ProductSearchingView.ItemsSource = db.Products.Where(p => p.Stock > 0);
+            }
+        }
+
+        private void Nov_Checked(object sender, RoutedEventArgs e)
+        {
+            if (OutStockCheckBox.IsChecked == true)
+            {
+                using var db = new AppDbContext();
+
+                ProductSearchingView.ItemsSource = db.Products.Where(p => p.Stock == 0);
+            }
+        }
+
+        private void refresh_Click(object sender, RoutedEventArgs e)
+        {
+            refreshButton.Content = "Refresh";
+            using (var db = new AppDbContext())
+            {
+                ProductSearchingView.ItemsSource = db.Products.ToList();
+            }
+        }
+    }
+}
---
+++ BarrocIntens/BarrocIntens/MaintenanceViews/ProductUsingView.xaml.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using BarrocIntens.Data;
+using Windows.Gaming.Input;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace BarrocIntens.MaintenanceViews
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ProductUsingView : Window
+    {
+        private int currentProductId;
+        public ProductUsingView(int productId)
+        {
+            this.InitializeComponent();
+
+            this.currentProductId = productId;
+
+            using var db = new AppDbContext();
+
+            var product = db.Products
+                .First(p => p.Id == productId);
+            nameInput.Text = product.Name;
+            descriptionInput.Text = product.Description;
+            stockInput.Text = product.Stock.ToString();
+            productCategoryInput.ItemsSource = db.ProductsCategories;
+            productCategoryInput.SelectedItem = product.ProductsCategory;
+        }
+
+        private void UsingProductButton_Click(object sender, RoutedEventArgs p)
+        {
+            using var db = new AppDbContext();
+            var product = db.Products
+                .First(p => p.Id == currentProductId);
+            product.Stock = int.Parse(stockInput.Text);
+
+            db.SaveChanges();
+            //nameInput.Text = string.Empty;
+            //descriptionInput.Text = string.Empty;
+            //priceInput.Text = "0";
+            //stockInput.Text = "0";
+            this.Close();
+        }
+
+        private void BackToStockButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
