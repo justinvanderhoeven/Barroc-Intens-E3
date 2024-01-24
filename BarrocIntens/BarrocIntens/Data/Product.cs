962f8109fd5179259e000eb7f96f6173685ad269
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2024-01-24 17:07:45 +0100

    Update Product.cs

--- BarrocIntens/BarrocIntens/Data/Product.cs
+++ BarrocIntens/BarrocIntens/Data/Product.cs
@@ -14,6 +14,8 @@ namespace BarrocIntens.Data
         public string ImagePath { get; set; } = null;
         public decimal Price { get; set; }
         public int Stock {  get; set; }
+        public int? MaintenanceAppointmentId { get; set; } = null;
+        public MaintenanceAppointment MaintenanceAppointment { get; set; }
         public ICollection<CustomInvoice> CustomInvoices { get; set; }
         public ICollection<CustomInvoiceProduct> CustomInvoiceProducts { get; set; }
         public ICollection<Contract> Contracts { get; set; }
